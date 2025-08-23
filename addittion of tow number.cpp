a9436a3d8b7ebc0cdf049e6bad50e399b887d5e1
SonuDangi07 <sonudangi0794@gmail.com>
2025-08-23 13:37:59 +0530

    Add files via upload

---
+++ addittion of tow number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	
+	int a,b,c;
+	
+	
+	cout<<"Enter the number : ";
+	cin>>a;
+    cout<<"Enter the number : ";
+    cin>>b;
+    
+    c = a+b;
+    cout<<"The sum tow number is: "<<c<<endl;
+    
+	
+	
+}
